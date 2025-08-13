a7ff3bbe5acf2c240f2e2c9349def6a731f0810e
Alex Cuadrado <alexkuadradomaso2003@gmail.com>
2025-08-13 16:07:23 +0200

    script.js: Modificamos funcion --> contacto() / salir()

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
-var botonInfo = document.getElementById("info-contacto");
-var cardContacto = document.getElementById("card-contacto");
 
-// Asociar la función al botón
-botonInfo.addEventListener('click', contacto);
-
-cardContacto.addEventListener('click', salir);
 
 function contacto() {
     var info = document.getElementById('card-contacto');
@@ -14,10 +8,20 @@ function contacto() {
     console.log('Botón clickeado');
 }
 
+function contacto2() {
+    var info = document.getElementById('card-contacto2');
+    var cardGeneral = document.getElementById('card-general');
+    cardGeneral.style.filter = 'blur(5px)';
+    info.style.display = 'block';
+    console.log('Botón clickeado');
+}
+
 function salir() {
     var info = document.getElementById('card-contacto');
+    var info2 = document.getElementById('card-contacto2')
     var cardGeneral = document.getElementById('card-general');
     info.style.display = "none";
+    info2.style.display = "none";
     cardGeneral.style.filter = 'blur(0)';
     console.log('Que esta pasando aquí?');
 }
